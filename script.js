db43f32e76bc721b963831550b18130cb6a3bfc3
Saurav <sauravsaurav0@gmail.com>
2025-06-03 13:29:07 +0530

    Generating Dynamic Cards from Dataset instead of hardcoded

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 import pokemonDataset from './pokemonDataset.js';
+
+for(let pokemon of pokemonDataset){
+
+    const individualCard = document.createElement("div");
+    individualCard.setAttribute('class','individualCard');
+
+    const image = document.createElement("img");
+    image.src = pokemon['sprite'];
+    image.setAttribute('class','image-Container');
+
+    const name = document.createElement('div');
+    name.innerText = pokemon['name'];
+    name.setAttribute('class','name-Container');
+
+    individualCard.append(image);
+    individualCard.append(name);
+    document.querySelector('#card-Container').append(individualCard);
+
+
+}
